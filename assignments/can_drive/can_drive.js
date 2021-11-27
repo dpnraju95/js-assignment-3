a9cf256d5976f4d54a40663d6edbdf849fb4a031
dpnraju95 <dpnraju95@gmail.com>
2021-11-27 16:17:11 +0530

    code canged

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,14 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if(hasDrivingLiscence==false || isTired==false || isSober==true) {
+        return String("You cannot drive")
+    }
+    else if((hasDrivingLiscence==true || isTired==false || isSober==true)){
+        return String("You can drive")
+    }
+    else if((hasDrivingLiscence==true || isTired==true || isSober==false)){
+        return String("You Shouldnot drive")
+    }
 }
 
 module.exports = CanDrive;
