6dcbc9cec50e6421ad95ca64eca3354498ab25db
dpnraju95 <dpnraju95@gmail.com>
2021-11-28 16:49:26 +0530

    deleted some files

--- assignments/bmi_calculator/bmi_calculator.js
+++
@@ -1,6 +0,0 @@
-// This function should return the BMI for a person 
-function BMICalculator(mass, height) {
-    // Write your code here
-}
-
-module.exports = BMICalculator;
--- assignments/can_drive/can_drive.js
+++
@@ -1,5 +0,0 @@
-function CanDrive(hasDrivingLiscence, isTired, isSober) {
-    // Write you code here
-}
-
-module.exports = CanDrive;
